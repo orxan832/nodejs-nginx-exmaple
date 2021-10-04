d569c968e6cf7f48fce79bfe92ce5eb3b24073ae
orxan832 <orxan832@gmail.com>
2021-10-04 20:52:44 +0400

    ...

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Orxan'));
+app.get('/', (req, res) => res.send(`İsteğin cevaplandığı port:  ${port}`));
 
 app.listen(5000, console.log('Backend was running'));
